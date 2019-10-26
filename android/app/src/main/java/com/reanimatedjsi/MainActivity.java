a70f9aca4ddb2af2400dd2512f94133b19e0fb37
Terry Sahaidak <s_tasssik_taras@ukr.net>
2019-10-26 14:17:25 +0300

    Init reanimated jsi

--- android/app/src/main/java/com/reanimatedjsi/MainActivity.java
+++ android/app/src/main/java/com/reanimatedjsi/MainActivity.java
@@ -1,8 +1,29 @@
 package com.reanimatedjsi;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.ReactInstanceManager;
+import com.swmansion.reanimated.ReanimatedModule;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends ReactActivity implements ReactInstanceManager.ReactInstanceEventListener {
+  @Override
+  public void onResume() {
+      super.onResume();
+      getReactInstanceManager().addReactInstanceEventListener(this);
+  }
+
+  @Override
+  public void onPause() {
+      super.onPause();
+      getReactInstanceManager().removeReactInstanceEventListener(this);
+  }
+
+  @Override
+  public void onReactContextInitialized(ReactContext context) {
+      ReanimatedModule reanimatedModule = context.getNativeModule(ReanimatedModule.class);
+
+       reanimatedModule.installJSI(context.getJavaScriptContextHolder().get());
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
